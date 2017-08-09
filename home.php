badc8748b7efbf59933cabc94ec20c8ce2790751
Stéphane Marcadet <smarcadet@clever-age.com>
2017-08-09 23:55:14 +0200

    add anchor link, add pattern for background, add sidebar styles

--- home.php
+++ home.php
@@ -1,23 +1,29 @@
 <?php get_header();
 
-    get_template_part( 'content/archive-header' );
+	get_template_part( 'content/archive-header' );
 
-    do_action( 'after_archive_header' ); ?>
+	do_action( 'after_archive_header' ); ?>
 
-    <div id="loop-container" class="loop-container">
+	<div class="mg-container-school">
+		<div id="loop-container" class="loop-container">
 
-        <?php
-        if ( have_posts() ) :
-            $i = 1;
-                while (have_posts() && $i < 4) :
-                    the_post();
-                    ct_period_get_content_template();
-                $i++;
-            endwhile;
-        endif;
-        ?>
-    </div>
+			<?php
+			if ( have_posts() ) :
+				$i = 1;
+					while (have_posts() && $i < 4) :
+						the_post();
+						ct_period_get_content_template();
+					$i++;
+				endwhile;
+			endif;
+			?>
+		</div>
 
-    <?php dynamic_sidebar('static_home'); ?>
+		<?php dynamic_sidebar('static_school'); ?>
+	</div>
+
+	<div class="mg-container-center">
+		<?php dynamic_sidebar('static_center'); ?>
+	</div>
 
 <?php get_footer(); ?>
