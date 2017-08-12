b38afb3e6d27fd1e00683283ad220ffea2f1c127
Stéphane Marcadet <smarcadet@clever-age.com>
2017-08-12 18:39:18 +0200

    add new fonts, end of post styling for large screens

--- content-archive.php
+++ content-archive.php
@@ -1,11 +1,9 @@
 <div <?php post_class(); ?>>
 	<article>
-		<?php do_action( 'archive_post_before' ); ?>
-		<?php ct_period_featured_image(); ?>
 		<div class="post-container">
 			<div class='post-header'>
 				<?php do_action( 'sticky_post_status' ); ?>
-				<h2 class='post-title'>
+				<h2 class='mg-title mg-title--alpha'>
 					<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
 				</h2>
 				<?php
@@ -15,6 +13,8 @@
 					echo '</div>';
 				?>
 			</div>
+			<?php do_action( 'archive_post_before' ); ?>
+			<?php ct_period_featured_image(); ?>
 			<div class="post-content">
 				<?php ct_period_excerpt(); ?>
 			</div>
