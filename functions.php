03966e16fdd0f178cd0effaa4ce224abbc6c6f7e
Stéphane Marcadet <smarcadet@clever-age.com>
2017-12-12 23:51:35 +0100

    add acf field in BO and remove page-in-widget module

--- functions.php
+++ functions.php
@@ -15,3 +15,4 @@ function my_theme_enqueue_script() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_script' );
+
--- home.php
+++ home.php
@@ -19,24 +19,157 @@
 			?>
 		</div>
 
-		<?php dynamic_sidebar('static_school'); ?>
+		<?php
+			// query for the about page
+			$your_query = new WP_Query( 'pagename=bloc-horaire-ecole' );
+			// "loop" through query (even though it's just one page) 
+			while ( $your_query->have_posts() ) : $your_query->the_post();
+				the_content();
+			endwhile;
+			// reset post data (important!)
+			wp_reset_postdata();
+		?>
 	</div>
 
 	<aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
 			<h1 class="screen-reader-text"><?php esc_html_e('Sidebar', 'period'); ?></h1>
-			<?php dynamic_sidebar( 'primary' ); ?>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=liens-ancres' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=78-2' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=medecin-scolaire' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=psychologue-scolaire' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=enseignant-referent' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=inspecteur-education' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
+			<section class="widget">
+				<?php
+					// query for the about page
+					$your_query = new WP_Query( 'pagename=maison-famille' );
+					// "loop" through query (even though it's just one page) 
+					while ( $your_query->have_posts() ) : $your_query->the_post();
+						the_content();
+					endwhile;
+					// reset post data (important!)
+					wp_reset_postdata();
+				?>
+			</section>
 	</aside>
 
 	<div id="asso" class="mg-container-asso">
-		<?php dynamic_sidebar('static_asso'); ?>
+		<div class="mg-col">
+			<?php
+				// query for the about page
+				$your_query = new WP_Query( 'pagename=associations' );
+				// "loop" through query (even though it's just one page) 
+				while ( $your_query->have_posts() ) : $your_query->the_post();
+					the_content();
+				endwhile;
+				// reset post data (important!)
+				wp_reset_postdata();
+			?>
+		</div>
+		<div class="mg-col">
+			<?php
+				// query for the about page
+				$your_query = new WP_Query( 'pagename=asso-representees' );
+				// "loop" through query (even though it's just one page) 
+				while ( $your_query->have_posts() ) : $your_query->the_post();
+					the_content();
+				endwhile;
+				// reset post data (important!)
+				wp_reset_postdata();
+			?>
+		</div>
 	</div>
 
 	<div id="hobbies" class="mg-container-hobbies">
-		<?php dynamic_sidebar('static_hobbies'); ?>
+		<?php
+			// query for the about page
+			$your_query = new WP_Query( 'pagename=bloc-centre-de-loisirs' );
+			// "loop" through query (even though it's just one page) 
+			while ( $your_query->have_posts() ) : $your_query->the_post();
+				the_content();
+			endwhile;
+			// reset post data (important!)
+			wp_reset_postdata();
+		?>
 	</div>
 
 	<div id="meal" class="mg-container-meal">
-		<?php dynamic_sidebar('static_meal'); ?>
+		<?php
+			// query for the about page
+			$your_query = new WP_Query( 'pagename=bloc-menu-cantine' );
+			// "loop" through query (even though it's just one page) 
+			while ( $your_query->have_posts() ) : $your_query->the_post();
+				the_content();
+			endwhile;
+			// reset post data (important!)
+			wp_reset_postdata();
+		?>
 	</div>
 
 <?php get_footer(); ?>
